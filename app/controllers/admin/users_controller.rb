5c27c1610837c001830cc0de0165375e55ee1e38
robertlyc <merlinlyc@gmail.com>
2014-07-02 22:02:40 +0800

    create user through admin backend

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,29 @@
-class Admin::UsersController < ApplicationController
-  before_action :authorize_admin!
-  
+class Admin::UsersController < Admin::BaseController
   def index
+    @users = User.order(:email)
   end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    params = user_params.dup
+    params[:password_confirmation] = params[:password]
+    @user = User.new(params)
+    
+    if @user.save
+      flash[:notice] = "User has been created."
+      redirect_to admin_users_path
+    else
+      flash.now[:alert] = "User has not been created."
+      render "new"
+    end
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name, :password, :password_confirmation)
+    end
+  
 end
