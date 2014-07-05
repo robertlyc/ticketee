839ac037ec24589768df017bd71cfc3112c7a8c8
robertlyc <merlinlyc@gmail.com>
2014-07-06 00:25:57 +0800

    add seed data

---
+++ spec/features/seeds_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+feature "Seed Data" do
+  scenario "The basic" do
+    load Rails.root + "db/seeds.rb"
+    user = User.where(email: "admin@example.com").first!
+    project = Project.where(name: "Ticketee Beta").first!
+  end
+end
