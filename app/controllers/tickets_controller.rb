5371ae498637a476783f3b830f728f1c82464245
robertlyc <merlinlyc@gmail.com>
2014-07-07 04:32:46 +0800

    add asset to upload

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -56,7 +56,7 @@ class TicketsController < ApplicationController
     end
     
     def ticket_params
-      params.require(:ticket).permit(:title, :description)
+      params.require(:ticket).permit(:title, :description, :asset)
     end
     
     def authorize_create!
